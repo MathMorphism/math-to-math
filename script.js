102ffeeeea86545aca62e62c0e1f5820796dc741
Tiffany Wong <tiffanywong@Tiffanys-MacBook-Pro.local>
2020-06-05 20:00:34 -0400

    add basic html, buttons working javascript

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('working');
+const start = document.querySelector('#start');
+const stop = document.querySelector('#stop');
+
+// start the speech
+start.addEventListener('click', () => {
+        start.style.display = 'none';
+        stop.style.display = 'block';
+});
+
+// end the speech
+stop.addEventListener('click', () => {
+        start.style.display = 'block';
+        stop.style.display = 'none';
+});
