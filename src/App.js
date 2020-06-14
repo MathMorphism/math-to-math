f538939d1e42d520d329d6db176424c5a26371ba
Tiffany Wong <tiffanywong@Tiffanys-MacBook-Pro.local>
2020-06-14 00:02:49 -0400

    moved from js to react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './styles/style.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    record: true
+  }
+
+  toggle = () => {
+    this.setState({
+      record: !this.state.record
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3> Words </h3>
+        <div className="words" contenteditable></div>
+        <h3> Mathematical Result </h3>
+        <div className="words result" contenteditable></div>
+        {this.state.record === true ?
+        <button className="start" onClick={this.toggle}> START </button> :
+        <button className="end" onClick={this.toggle}> END </button>
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
