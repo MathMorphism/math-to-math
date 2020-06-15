10c12de3d05aedaf90067ca3777f73d1a61f5c69
Tiffany Wong <tiffanywong@Tiffanys-MacBook-Pro.local>
2020-06-14 20:21:18 -0400

    delete html from app

--- src/App.js
+++ src/App.js
@@ -3,32 +3,11 @@ import SpeechRecognition from './SpeechRecognition'
 import './styles/style.scss';
 
 class App extends Component {
-  state = {
-    record: true
-  }
-
-  toggle = () => {
-    this.setState({
-      record: !this.state.record
-    })
-  }
-
   render() {
     return (
-      <div>
-        <h3> Words </h3>
-        <div className="words" contenteditable></div>
-        <h3> Mathematical Result </h3>
-        <div className="words result" contenteditable></div>
-        {this.state.record === true ?
-        <button className="start" onClick={this.toggle}> START </button> :
-        <button className="end" onClick={this.toggle}> END </button>
-        }
-
         <div className="App">
           <SpeechRecognition />
         </div>
-      </div>
     )
   }
 }
